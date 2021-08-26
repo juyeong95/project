f3ba0929a3dacfbafebca860a363577b8b7afd65
kwanghun <s5095940@naver.com>
2021-08-26 17:45:07 +0900

    첫번째 커밋

---
+++ src/kwanghun/Testkwanghun.java
@@ -0,0 +1,8 @@
+package kwanghun;
+
+public class Testkwanghun {
+	public void display() {
+		System.out.println("광훈 기능 완성");
+	}
+
+}
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,10 +2,14 @@ package main;
 
 import java.util.Scanner;
 
+import kwanghun.Testkwanghun;
+
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num;
+		Testkwanghun tkh = new Testkwanghun();
 		while(true) {
 			System.out.println("1.이주영님 2.성기룡님 3.설지희님 4.서광훈님 5.송영관님");
 			num=input.nextInt();
@@ -13,7 +17,8 @@ public class MainClass {
 			case 1: break;
 			case 2: break;
 			case 3: break;
-			case 4: break;
+			case 4: tkh.display();
+				break;
 			case 5: break;
 			}
 		}
