1fde2a0b2700a9e502b2958f174f780c529fabf4
juyoung <bn0406@naver.com>
2021-08-26 16:21:12 +0900

    플젝생성

---
+++ src/main/MainClass.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.ÀÌÁÖ¿µ´Ô 2.¼º±â·æ´Ô 3.¼³ÁöÈñ´Ô 4.¼­±¤ÈÆ´Ô 5.¼Û¿µ°ü´Ô");
+			num=input.nextInt();
+			switch(num) {
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			}
+		}
+	}
+}
